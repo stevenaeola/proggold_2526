134ba5fd90c88248b8c56151861df11d21083401
dcs0spb <s.p.bradley@durham.ac.uk>
2026-01-19 14:51:04 +0000

    search for characters and rearrange API

--- cars/server.js
+++ cars/server.js
@@ -9,10 +9,19 @@ app.get('/', function(req, resp){
   resp.send('Hello from Radiator Springs')
 })
 
-app.get('/character/:num', function(req,resp){
+app.get('/character/detail/:num', function(req,resp){
+    console.log("getting an individual character")
+
     let num = parseInt(req.params.num)
     let char = characters[num]
     resp.send(char)
+})
+
+app.get('/character/search', function(req,resp){
+  console.log("seraching for lambs")
+  let search_term = req.query.search_term
+  let results = characters.filter(item => item.includes(search_term))
+  resp.send(results)
 })
 
 app.get('/random/:max', function(req, resp){
@@ -21,6 +30,14 @@ app.get('/random/:max', function(req, resp){
   console.log('Max via url is ' + max + ' rand is ' + rand)
   resp.send('' + rand)
 })
+
+app.get('/r', function(req, resp){
+  max = parseInt(req.query.max)
+  rand = Math.floor(Math.random()*max) +1
+  console.log('Max via query is ' + max + ' rand is ' + rand)
+  resp.send('' + rand)
+})
+
 
 app.listen(8090)
 
