a5da0742ad3c82e292a5fd78dd452ef3d888cd02
dcs0spb <s.p.bradley@durham.ac.uk>
2026-02-12 10:38:59 +0000

    Search form works. Closes #4

--- cars/client/index.js
+++ cars/client/index.js
@@ -10,6 +10,8 @@ search_button.addEventListener('click', function(event) {
 
 function displayCars(carList){
   console.log(carList)
+  let row1 = document.getElementById('row1')
+  row1.innerHTML= ""
   for(let car of JSON.parse(carList)){
     console.log(car.name)
 
@@ -26,7 +28,6 @@ function displayCars(carList){
                 </div>
               </div>
           </div>`
-    let row1 = document.getElementById('row1')
     row1.innerHTML+=carString
   }
 }
@@ -35,6 +36,16 @@ window.addEventListener("DOMContentLoaded", async function(event){
   let response = await fetch('http://127.0.0.1:8090/character/list')
   let carlist = await response.text()
   displayCars(carlist)
+})
 
-
+const search_form = document.getElementById("search_form")
+search_form.addEventListener("input", async function(event){
+ // alert ("input on form")
+  const search_box = document.getElementById('search_term')
+  let search_term = search_box.value
+ // alert("search for" + search_term)
+  let response = await fetch(`http://127.0.0.1:8090/character/search?search_term=${search_term}`)
+  let carlist = await response.text()
+  displayCars(carlist)
+// http://127.0.0.1:8090/character/search"
 })
