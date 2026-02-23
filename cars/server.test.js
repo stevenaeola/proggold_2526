eb816753777ed2d7d8d2f1edf13ccf7dafe831c8
dcs0spb <s.p.bradley@durham.ac.uk>
2026-02-23 14:56:15 +0000

    Set up jest testing: two tests pass

---
+++ cars/server.test.js
@@ -0,0 +1,51 @@
+
+'use strict';
+
+const request = require('supertest');
+const app = require('./app');
+
+describe('Test the character service', () => {
+    test('GET /character/list succeeds', () => {
+        return request(app)
+	    .get('/character/list')
+	    .expect(200);
+    });
+
+    test('GET /character/list returns JSON', () => {
+        return request(app)
+	    .get('/character/list')
+	    .expect('Content-type', /json/);
+    });
+
+    test('GET /thing/list includes red hair', () => {
+        return request(app)
+	    .get('/thing/list')
+	    .expect(/red hair/);
+    });
+
+    test('GET /thing/1 succeeds', () => {
+        return request(app)
+	    .get('/thing/1')
+	    .expect(200);
+    });
+
+    test('GET /thing/1 returns JSON', () => {
+        return request(app)
+	    .get('/thing/1')
+	    .expect('Content-type', /json/);
+    });
+
+    test('GET /thing/1 includes 40', () => {
+        return request(app)
+	    .get('/thing/1')
+	    .expect(/40/);
+    });
+
+    test('POST /thing/add succeeds', () => {
+        const params = {'newthing': 'TechUp'};
+        return request(app)
+        .post('/thing/add')
+        .send(params)
+	    .expect(200);
+    });
+});
